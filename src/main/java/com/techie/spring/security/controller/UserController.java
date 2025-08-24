710a4901cc7e3be53a04a95722c1467daaeddff3
harikrishna t <harikrishna.tekkam@gmail.com>
2025-08-24 09:57:21 +0530

    added code for customized authentication manager

--- src/main/java/com/techie/spring/security/controller/UserController.java
+++ src/main/java/com/techie/spring/security/controller/UserController.java
@@ -28,10 +28,12 @@ public class UserController {
 
     @PostMapping("/login")
     public String login(@RequestBody User user) {
-        var u = userRepository.findByUsername(user.getUsername());
-        if (!Objects.isNull(u))
-            return "success";
-        else
-            return "failure";
+
+        return userService.verify(user);
+//        var u = userRepository.findByUsername(user.getUsername());
+//        if (!Objects.isNull(u))
+//            return "success";
+//        else
+//            return "failure";
     }
 }
--- src/main/java/com/techie/spring/security/service/UserService.java
+++ src/main/java/com/techie/spring/security/service/UserService.java
@@ -2,8 +2,13 @@ package com.techie.spring.security.service;
 
 import com.techie.spring.security.entity.User;
 import com.techie.spring.security.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class UserService {
@@ -12,14 +17,30 @@ public class UserService {
 
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    private final AuthenticationManager authenticationManager;
+
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder, AuthenticationManager authenticationManager) {
         this.userRepository = userRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.authenticationManager = authenticationManager;
     }
 
     public User register(User user) {
         user.setPassword(bCryptPasswordEncoder.
                 encode(user.getPassword()));
         return userRepository.save(user);
+    }
+
+    public String verify(User user) {
+        Authentication authenticate = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        user.getUsername(), user.getPassword()
+                ));
+
+        //var u = userRepository.findByUsername(user.getUsername());
+        if (authenticate.isAuthenticated())
+            return "8179268257";
+        else
+            return "failure";
     }
 }
